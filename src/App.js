cc434a85c0501ce742c286c942744f9aa1b9a012
VladVinichenko <vinnyxxxl925@gmail.com>
2022-01-27 12:33:00 +0200

    all

--- src/App.js
+++ src/App.js
@@ -1,51 +1,47 @@
 import Section from "./components/Section/Section";
 import FeedbackOptions from "./components/FeedbackOptions/FeedbackOptions";
 import Statistics from "./components/Statistics/Statistics";
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Fragment } from "react/cjs/react.production.min";
+import propTypes from "prop-types";
 
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
+
+function App() {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incrementFeedback = evt => {
+    evt === 'good' && setGood(good + 1)
+    evt === 'neutral' && setNeutral(neutral + 1)
+    evt === 'bad' && setBad(bad + 1)
   }
 
-  incrementFeedback = evt => {
-    this.setState((prevState) => {
-      const typeFeedback = evt
-      const newState = {}
-      newState[typeFeedback] = prevState[typeFeedback] + 1
-      return {
-        ...newState
-      }
-    })
-
+  const countTotalFeedback = () => {
+    return good + neutral + bad
   }
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, value) => acc + value)
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round([good / countTotalFeedback() * 100])
   }
 
-  countPositiveFeedbackPercentage = () => {
-    return Math.round([this.state.good / this.countTotalFeedback() * 100])
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <Section title='Statistics'>
-          <FeedbackOptions options={this.state} onLeaveFeedback={this.incrementFeedback} />
-        </Section>
-        <Section title='Please leave feedback'>
-          <Statistics good={this.state.good} neutral={this.state.neutral} bad={this.state.bad} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()}></Statistics>
-        </Section>
-      </Fragment>
-    )
-  }
+  return (
+    <Fragment>
+      <Section title='Statistics'>
+        <FeedbackOptions options={{ good: good, neutral: neutral, bad: bad }} onLeaveFeedback={incrementFeedback} />
+      </Section>
+      <Section title='Please leave feedback'>
+        <Statistics good={good} neutral={neutral} bad={bad} total={countTotalFeedback()} positivePercentage={countPositiveFeedbackPercentage()}></Statistics>
+      </Section>
+    </Fragment>
+  )
 }
 
-
+App.propTypes = {
+  good: propTypes.number,
+  neutral: propTypes.number,
+  bad: propTypes.number,
+}
 
 export default App
